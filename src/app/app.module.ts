d901079780746ac3a361436377c5809ff19da44e
4garfield <songbintaochina@gmail.com>
2020-12-03 16:27:10 +0800

    add material & flex-layout

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 import { TransferHttpCacheModule } from '@nguniversal/common';
+import { FlexLayoutModule } from '@angular/flex-layout';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -12,6 +14,8 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule.withServerTransition({ appId: 'ngSampleApp' }),
     TransferHttpCacheModule,
+    BrowserAnimationsModule,
+    FlexLayoutModule,
     AppRoutingModule
   ],
   providers: [],
